cf57aac0ea4de3aff5c66d2c3bbe9c47f41aaddf
Ashish <Ashish@DESKTOP-IHL8038>
2022-10-01 16:10:46 +0530

    adding new two test cases

--- Com.demoProject/src/test/java/com/tests/DemoTest.java
+++ Com.demoProject/src/test/java/com/tests/DemoTest.java
@@ -7,5 +7,15 @@ public class DemoTest {
 	 public void test01(){
 		 System.out.println("Hello World");
 	 }
+	 
+	 @Test
+	 public void test02(){
+		 System.out.println("Hello India");
+	 }
+	 
+	 @Test
+	 public void test03(){
+		 System.out.println("Hello Pune");
+	 }
 
 }
